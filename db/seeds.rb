64e0761f265c479922c051debbadaf2a6728ff02
Apprentice <apprentice@dbc22.local>
2013-05-14 15:10:27 -0700

    spec tests pass 'posts_controller_spec'

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Post.create(title: "Test1", content: "This is a test")
+# Post.create(title: "Test2", content: "This is a test")
+# Post.create(title: "Test3", content: "This is a test")
+# Post.create(title: "Test4", content: "This is a test")
+# Post.create(title: "Test5", content: "This is a test")
+# Post.create(title: "Test6", content: "This is a test")
