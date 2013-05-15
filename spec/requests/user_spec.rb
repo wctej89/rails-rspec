8675618bea7b40b3a3efe75f0ae4af8abeef36f8
Apprentice <apprentice@devbootcamp.com>
2013-05-14 19:44:04 -0700

    pass view test of user_spec.rb on posts_path

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,21 +1,29 @@
 require 'spec_helper'
 
-describe 'User' do
+describe 'User', :js => true do
+  
   context "on homepage" do
+  
+    let!(:post){ Post.create!(title:"New Post!", content:"a great post") }
+
     it "sees a list of recent posts titles" do
-      pending
+      visit posts_path
+      debugger
+      page.should have_content(post.title)
       # given a user and a list of posts
       # user visits the homepage
       # user can see the posts titles
     end
     it "can not see bodies of the recent posts" do
-      pending
+      visit posts_path
+      page.should_not have_content("a great post")
       # given a user and a list of posts
       # user visits the homepage
       # user should not see the posts bodies
     end
     it "can click on titles of recent posts and should be on the post show page" do
-      pending
+      visit posts_path
+      click_link("New Post!")
       # given a user and a list of posts
       # user visits the homepage
       # when a user can clicks on a post title they should be on the post show page
